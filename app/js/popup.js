a9d634e697cbadf4bcde41dfc408ce3cfeebd050
Christian Haudum <christian@rjdj.me>
2014-04-09 18:55:19 +0200

    display record information

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,25 +1,10 @@
-    // <tr>
-    //   <td><button class="">Pause</button></td>
-    //   <td class="name">Crate</td>
-    //   <td class="info">
-    //     <ul>
-    //       <li>http://demo.crate.io</li>
-    //       <li>Load: 0.12</li>
-    //       <li>100.0% available</li>
-    //       <li>50.0% replicated</li>
-    //       <li>25.28M records</li>
-    //     </ul>
-    //   </td>
-    // </tr>
-
-
 var createTableRow = function(cluster){
     var row = $('<tr id="cluster-'+cluster._id+'">' +
         '<td><button class="pause"></button></td>' +
-        '<td class="name">--</td>' +
+        '<td><span class="name">--</span></td>' +
         '<td class="info">' +
         '<ul>' +
-        '<li><a href="#">--</a></li>' +
+        '<li><a href="#" target="_blank">--</a></li>' +
         '<li>Load: <span class="load">--</span></li>' +
         '<li><span class="available">--</span> available</li>' +
         '<li><span class="replicated">--</span> replicated</li>' +
@@ -29,24 +14,21 @@ var createTableRow = function(cluster){
         '</tr>');
     $("#clusters").append(row);
 };
-var updateTableRow = function(cluster){
-    // var row = $("#cluster-"+cluster._id);
-    // $(".name", row).text(cluster.info.name);
-    // $("a", row).attr("href", cluster.url+"/admin");
-};
-
-var fetchInfo = function fetchInfo(cluster) {
-    getClusterInfo(cluster).done(function(info){
-        cluster.info = info;
-        updateTableRow(cluster);
-    });
-}
 
 document.addEventListener("DOMContentLoaded", function(){
     var background = chrome.extension.getBackgroundPage();
     for (var i=0; i<background.clusters.length; i++) {
         var cluster = background.clusters[i];
         createTableRow(cluster);
-        fetchInfo(cluster);
+        getClusterInfo(cluster).done(function(info){
+            cluster.info = info;
+            var row = $("#cluster-"+cluster._id);
+            $(".name", row).text(cluster.info.name).addClass(cluster.state.name);
+            $("a", row).attr("href", cluster.url + "/admin").text(cluster.url);
+            $(".load", row).text(cluster.info.load[0].toFixed(2));
+            $(".available", row).text(cluster.tableInfo.available_data.toFixed(1)+"%");
+            $(".replicated", row).text(cluster.tableInfo.replicated_data.toFixed(1)+"%");
+            $(".records", row).text(cluster.tableInfo.records_total);
+        });
     }
 }, false);
